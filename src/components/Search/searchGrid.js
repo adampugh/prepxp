e58f8039f46eff6c12d93a2bbb4fcb7414fc3dbd
Adam Pugh <adampugh@zoho.com>
2018-03-21 22:49:30 +0000

    Added styles to searchGrid

--- src/components/Search/searchGrid.js
+++ src/components/Search/searchGrid.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const searchGrid = (props) => (
-    <div className="listPage__questions">
+    <div className="searchGrid__lists">
         <div className="container-s">
             {props.filteredLists.length > 0 ? props.filteredLists.map((list, index) => (
-                <div className="dashboard__grid__lists__item" key={`${list.title + index}`}>
+                <div className="searchGrid__list__item" key={`${list.title + index}`}>
                     <div>    
                         <h2>{list.title}</h2>
                         <p>{list.questions.length} Questions</p>
                     </div>
-                    <h2><i className="fas fa-plus"></i></h2>
+                    <h2 className="searchGrid__list__icon"><i className="fas fa-plus"></i></h2>
                 </div>
             )) : <h2> Your search did not return any results</h2>}
         </div>
